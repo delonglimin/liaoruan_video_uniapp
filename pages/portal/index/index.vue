12c19febb745e44b818b4183a0d24ab797ff482f
许得龙 <xudelong@xudelongdeMacBook-Pro.local>
2024-01-12 17:22:31 +0800

    init

---
+++ pages/portal/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		index
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/portal/rank/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		rank
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
